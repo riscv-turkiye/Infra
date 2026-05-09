cb76f3584fc2ae8cd65a900f656f82fc7656b953
Alper Çelik <alper@alper-celik.dev>
2026-05-09 20:22:51 +0300

    hello world nginx

--- hosts/riscv-tr-1/nginx.nix
+++ hosts/riscv-tr-1/nginx.nix
@@ -6,5 +6,15 @@
 {
   services.nginx = {
     enable = true;
+    virtualHosts."_" = {
+      default = true;
+
+      locations."/" = {
+        return = "200 'Hello from IP: $server_addr\n'";
+        extraConfig = ''
+          add_header Content-Type text/plain;
+        '';
+      };
+    };
   };
 }
